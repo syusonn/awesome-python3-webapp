3da74e9cff438f71a3269c4994fb7b5c36cdf0c9
syuson <595988046@qq.com>
2017-07-21 17:55:21 +0800

    增加自启辅助工具pymonitor.py

--- www/handlers.py
+++ www/handlers.py
@@ -77,7 +77,7 @@ async def cookie2user(cookie_str):
 def index(request):
 	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 	blogs = [
-		Blog(id='1',name='Test Bolg',summary=summary,created_at=time.time()-120),
+		Blog(id='1',name='Test Blog',summary=summary,created_at=time.time()-120),
 		Blog(id='2',name='Something New',summary=summary,created_at=time.time()-3600),
 		Blog(id='3',name='Learn Swift',summary=summary,created_at=time.time()-7200)
 	]
