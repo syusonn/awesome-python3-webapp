2ab194f2023b0b071d53dc60ec53fd5e172e3ff1
syuson <595988046@qq.com>
2017-07-21 09:42:40 +0800

    config setting files import

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'syuson'
+
+configs = {
+	'db' : {
+
+		'host' : '127.0.0.1',
+		'port' : 3306,
+		'user' : 'root',
+		'password' : 'root',
+		'database' : 'awesome'
+	},
+	'session':{
+		'secret' : 'Awesome'
+	}
+
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'syuson'
+
+configs = {
+
+	'db' : {
+		'host' : '192.168.110.168'
+	}
+
+}
