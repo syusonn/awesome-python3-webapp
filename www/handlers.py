5acc264420bfb1c4c6a7823419429205528342bd
syuson <595988046@qq.com>
2017-07-21 11:03:25 +0800

    构建前端

--- www/handlers.py
+++ www/handlers.py
@@ -13,8 +13,13 @@ from models import User,Comment,Blog,next_id
 
 @get('/')
 async def index(request):
-	users = await User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1',name='Test Bolg',summary=summary,created_at=time.time()-120),
+		Blog(id='2',name='Something New',summary=summary,created_at=time.time()-3600),
+		Blog(id='3',name='Learn Swift',summary=summary,created_at=time.time()-7200)
+	]
 	return{
-		'__template__' : 'test.html',
-		'users' : users
+		'__template__' : 'blogs.html',
+		'blogs' : blogs
 	}
